c79452a36aaec53d230161051b5801b291387db7
Alessandro Marega <ale_marega@hotmail.it>
2021-10-13 21:41:53 +0200

    inizio progetto

---
+++ js/script.js
@@ -0,0 +1,30 @@
+export default {
+  name: "App",
+  data() {
+    return {
+      elapsedTime: 0,
+      timer: undefined
+    };
+  },
+  computed: {
+    formattedElapsedTime() {
+      const date = new Date(null);
+      date.setSeconds(this.elapsedTime / 1000);
+      const utc = date.toUTCString();
+      return utc.substr(utc.indexOf(":") - 2, 8);
+    }
+  },
+  methods: {
+    start() {
+      this.timer = setInterval(() => {
+        this.elapsedTime += 1000;
+      }, 1000);
+    },
+    stop() {
+      clearInterval(this.timer);
+    },
+    reset() {
+      this.elapsedTime = 0;
+    }
+  }
+};
